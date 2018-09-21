2369ada14be66fc7d022b0dcff7438ffbf90332f
HalfMileFlower <harry08010@tamu.edu>
2018-09-20 20:41:56 -0500

    lc-170 two sum III

---
+++ 170_two_sum_III.java
@@ -0,0 +1,35 @@
+public class TwoSum {
+    
+    Map<Integer, Integer> map = new HashMap<>();
+    /**
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void add(int number) {
+        // write your code here
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+        }
+        
+    }
+
+    /**
+     * @param value: An integer
+     * @return: Find if there exists any pair of numbers which sum is equal to the value.
+     */
+    public boolean find(int value) {
+        // write your code here
+        for (int c : map.keySet()) {
+            if (map.containsKey(value - c)) {
+                if ((value - c == c && map.get(c) >= 2) || value - c != c) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// Notes: handling for elements added more than once!
