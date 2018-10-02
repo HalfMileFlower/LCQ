0b885eb77168726bf634dc70ebc2e4367cb6bf18
HalfMileFlower <harry08010@tamu.edu>
2018-10-02 01:05:43 -0500

    lc-658 find k closest elements leftbound

--- 658_find_k_closest_elements.java
+++ 658_find_k_closest_elements.java
@@ -50,3 +50,24 @@ class Solution {
         return left;
     }
 }
+
+// leftBound
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        int left = 0;
+        int right = arr.length - k - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (Math.abs(x - arr[mid]) > Math.abs(x - arr[mid + k])) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            res.add(arr[left + i]);
+        }
+        return res;
+    }
+}
