c450139f3822f2b9ade13372b2378b9119b6489a
HalfMileFlower <weitongfeng@hotmail.com>
2018-10-03 16:43:20 -0500

    lc-680 valid palindrome II

---
+++ 680_valid_palindrome_II.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        } 
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+// Notes: After finding s.charAt(left) != s.charAt(right) check if still palindrome after moving left or right
