7bc59e9e77957df16825d902f00b373fa30c5ea6
HalfMileFlower <weitongfeng@hotmail.com>
2018-10-07 15:59:53 -0500

    lc-876 middle of linked list

---
+++ 876_middle_of_linked_list.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode quick = head;
+        ListNode slow = head;
+        
+        while (quick != null && quick.next != null) {
+            quick = quick.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+}
+
+// Notes: quick != null && quick.next != null.
