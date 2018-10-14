81d01ed1579b439a1ce0e2f1fbd60002b0d00a8f
HalfMileFlower <weitongfeng@hotmail.com>
2018-10-14 02:17:40 -0500

    lc-3 longest substring without repeating characters - hashset solution

--- 3_longest_substring_without_repeating_characters.java
+++ 3_longest_substring_without_repeating_characters.java
@@ -25,3 +25,33 @@ class Solution {
 // Each time we meet a repeating character, move the slow one(tracking the head of non-repeating string)
 // TC: O(n) SC: O(n)
 
+// HashSet Solution
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        } 
+        
+        int max = Integer.MIN_VALUE;
+        int longestStart = 0;
+        
+        Set<Character> set = new HashSet<>();
+        int pointer = 0;
+        while (pointer < s.length()) {
+            if (set.contains(s.charAt(pointer))) {
+                set.remove(s.charAt(longestStart));
+                longestStart++;
+            } else {
+                set.add(s.charAt(pointer));
+                pointer++;
+                max = Math.max(max, set.size());
+            }
+        }
+        return max;
+    }
+}
+
+// Notes: use hashset to record unique characters in the result string
+// if a character is not in set, put it in; else remove from head until the repeated character is removed
+// calculate the max of set size
+// TC: O(n) SC: O(n)
