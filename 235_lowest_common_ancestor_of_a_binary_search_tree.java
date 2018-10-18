ffb213b0a6e4e13e3fff799e5d40dedc85136eb5
HalfMileFlower <weitongfeng@hotmail.com>
2018-10-18 03:18:04 -0500

    lc-235 lowest common ancestor of a binary search tree - recursive solution

--- 235_lowest_common_ancestor_of_a_binary_search_tree.java
+++ 235_lowest_common_ancestor_of_a_binary_search_tree.java
@@ -19,5 +19,23 @@ class Solution {
 }
 
 // Notes: if p, q < left || right < p, q, update root
-// TC: O(logn) SC: O(1)
+// TC: O(h) SC: O(1) h is the height of the tree
 // (in case of overflow, I'd do (root.val - (long)p.val) * (root.val - (long)q.val))
+
+// Recursive Solution
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
+
+// Notes: if p, q at left of root, lca(left, p, q)
+// if p, q at right of root, lca(right, p, q)
+// else return root
+// TC: O(h) SC: O(1) h is the height of the tree
