2c5444409813043217b15ffc090e59ae8d98929d
HalfMileFlower <weitongfeng@hotmail.com>
2018-10-23 00:11:56 -0500

    lc-160 intersection of two linked lists

---
+++ 160_intersection_of_two_linked_lists.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
+
+// Notes: make a -> headB & b -> headA when a / b reach end, so that each pointer passes same length
+// TC: O(n + m) n, m are lengths of lists  SC: O(1)
