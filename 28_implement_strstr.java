92cd6fc47b072596433302960f9681a87581d0fd
HalfMileFlower <weitongfeng@hotmail.com>
2018-10-23 00:29:20 -0500

    lc-28 implement strstr - brutal solution

--- 28_implement_strstr.java
+++ 28_implement_strstr.java
@@ -1,29 +1,19 @@
-// Notes: 
-/* 
- * Rabin-Karp Algorithm:
- * Use hash function to convert a string to a integer
- * Use 31 to be the base
- * In case the result overflow, calculate the remainder with 10^6 or higher number with long int, the bigger you use as the diviser, the more strings you can demonstrate.
- * Each time you should move one character, and calculate the remainder. Keep the modularing process if any number overflows.
- * Double check if the substring really equals to the target.
- * Main idea: if the two values from a same hash function are not the same, then the origin strings must be different.
- * Key point: implementation of hash function
- */
-
 // Brutal Solution
 class Solution {
     public int strStr(String haystack, String needle) {
-        int hLength = haystack.length();
-        int nLength = needle.length();
-        if (haystack == null || nLength > hLength) {
-            return -1;
-        }
-        if (needle == null || nLength == 0) {
+        if (needle == null || needle.length() == 0) {
             return 0;
         }
         
-        for (int i = 0; i <= hLength - nLength; i++) {
-            if (haystack.substring(i, i + nLength).equals(needle)) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            for (int k = 0; k < needle.length(); k++) {
+                if (needle.charAt(k) != haystack.charAt(i + j)) {
+                    break;
+                }
+                j++;
+            }
+            if (j == needle.length()) {
                 return i;
             }
         }
@@ -31,52 +21,6 @@ class Solution {
     }
 }
 
-// Rabin Karp
-class Solution {
-    
-    static int BASE = 1000000;
-    public int strStr(String haystack, String needle) {
-        
-        int hLength = haystack.length();
-        int nLength = needle.length();
-        if (haystack == null || hLength < nLength) {
-            return -1;
-        }
-        
-        if (needle == null || needle.length() == 0) {
-            return 0;
-        }
-        
-        int power = 1;
-        for (int i = 0; i < nLength; i++) {
-            power = (power * 31) % BASE;
-        }
-        
-        int needleCode = 0;
-        int targetCode = 0;
-        for (int i = 0; i < nLength; i++) {
-            needleCode = (needle.charAt(i) + needleCode * 31) % BASE;
-        }
-        
-        for (int j = 0; j < hLength; j++) {
-            targetCode = (haystack.charAt(j) + targetCode * 31) % BASE;
-            if (j < nLength - 1) {
-                continue;
-            }
-            
-            if (j > nLength - 1) {
-                targetCode = targetCode - (power * haystack.charAt(j - nLength)) % BASE;
-                if (targetCode < 0) {
-                    targetCode += BASE;
-                }
-            }
-            
-            if (targetCode == needleCode) {
-                if (haystack.substring(j - nLength + 1, j + 1).equals(needle)) {
-                    return j - nLength + 1;
-                }
-            }
-        }
-        return -1;
-    }
-}
+// Notes: TC: O(m * n) SC: O(1)
+
+// Rabin-Karp
