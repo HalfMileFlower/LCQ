f153c819c71d47a488c8f6c95f3cecdd46ac5b4f
HalfMileFlower <weitongfeng@hotmail.com>
2018-11-05 01:01:20 -0600

    lc-23 merge k sorted lists - Merge-like-tree Solution

--- 23_merge_k_sorted_lists.java
+++ 23_merge_k_sorted_lists.java
@@ -46,3 +46,60 @@ class Solution {
 // pay attention to null ListNodes !!!
 // TC: O(nlogk) - n: number of all listNodes, k: length of lists
 // SC: O(k)
+
+// Merge-like-tree Solution
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return mergeHelper(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode mergeHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeHelper(lists, start, mid);
+        ListNode right = mergeHelper(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode pointer = new ListNode(0);
+        ListNode dummy = pointer;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pointer.next = l1;
+                l1 = l1.next;
+            } else {
+                pointer.next = l2;
+                l2 = l2.next;
+            }
+            pointer = pointer.next;
+        }
+        
+        if (l1 != null) {
+            pointer.next = l1;
+        }
+        
+        if (l2 != null) {
+            pointer.next = l2;
+        }
+        
+        return dummy.next;
+    }
+}
+
+// Notes: merge two lists and merge the result list with another similar list
+//        O
+//      /   \
+//     O     O
+//    / \   / \
+//   O   O O   O
+// mergeHelper to implement the above tree like merging process
+// if start == end, return lists[start]
+// TC: O(nlogk)  SC: O(1)
