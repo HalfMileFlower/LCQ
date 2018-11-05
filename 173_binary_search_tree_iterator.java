69dbd056dc818fa40f24943b5bd0f3002ebbbede
HalfMileFlower <weitongfeng@hotmail.com>
2018-11-05 14:53:39 -0600

    lc-173 binary search tree iterator

---
+++ 173_binary_search_tree_iterator.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+    Stack<TreeNode> stack = new Stack<>();
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.empty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode top = stack.pop();
+        pushAll(top.right);
+        return top.val;
+    }
+    
+    private void pushAll(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
+ // Notes: use stack to store least at the top, very similar to inorder traversal
+ // TC: O(1) for each of next() and hasNext() - average. SC: O(h)
+ 
