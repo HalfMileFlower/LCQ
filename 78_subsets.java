eb2e9d7bf362dc739ba663b0ee56ebc6c547ed44
HalfMileFlower <weitongfeng@hotmail.com>
2018-11-10 02:34:23 -0600

    lc-78 subsets

---
+++ 78_subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+        
+        result.add(new ArrayList<>());
+        findSubSets(0, nums, result, new ArrayList<>());
+        return result;
+    }
+    
+    private void findSubSets(int start, int[] nums, List<List<Integer>> result, ArrayList<Integer> list) {
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            result.add(new ArrayList<>(list));
+            findSubSets(i + 1, nums, result, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
+
+// Notes: backtracking, pay attention to the empty set []
+// TC: O(n!)   SC: O(n) - stack
